bfc792364ef8784afe9742a63ffed22e3e9b817c
stolzz0613 <andres0613@utp.edu.co>
2020-07-25 23:38:57 -0500

    Updates

---
+++ precache-manifest.72ca3ade084935e1e014f7afd1b933a0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07430249555356f4870a8d020e17da8e",
+    "url": "/DrinkRecipes/index.html"
+  },
+  {
+    "revision": "9bfcbfd251623d933526",
+    "url": "/DrinkRecipes/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "b9872db8d270b28b8a7b",
+    "url": "/DrinkRecipes/static/css/main.61c545f5.chunk.css"
+  },
+  {
+    "revision": "9bfcbfd251623d933526",
+    "url": "/DrinkRecipes/static/js/2.19e2f016.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DrinkRecipes/static/js/2.19e2f016.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9872db8d270b28b8a7b",
+    "url": "/DrinkRecipes/static/js/main.03275059.chunk.js"
+  },
+  {
+    "revision": "ea8250ea8ae08bab7ae5",
+    "url": "/DrinkRecipes/static/js/runtime-main.ef968183.js"
+  }
+]);
