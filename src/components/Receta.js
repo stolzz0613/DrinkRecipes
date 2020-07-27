7048faf3bc1d889fdc9d51bc3153abed42e0ff0c
stolzz0613 <andres0613@utp.edu.co>
2020-07-27 01:03:22 -0500

    modal

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -1,9 +1,47 @@
-import React, { useContext } from 'react';
-import { ModalContext } from "../context/ModalContext";
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useContext, useState } from 'react';
 import Modal from '@material-ui/core/Modal';
+import { makeStyles } from '@material-ui/core/styles';
+import { ModalContext } from "../context/ModalContext";
+
+
+
+
+function getModalStyle() {
+    const top = 50;
+    const left = 50;
+
+    return {
+        top: `${top}%`,
+        left: `${left}%`,
+        transform: `translate(-${top}%, -${left}%)`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        position: 'absolute',
+        width: 400,
+        backgroundColor: theme.palette.background.paper,
+        boxShadow: theme.shadows[5],
+        padding: theme.spacing(2, 4, 3),
+    },
+}));
+
 
 const Receta = ({ receta }) => {
+
+    const [modalStyle] = useState(getModalStyle);
+    const [open, setOpen] = useState(false);
+
+    const classes = useStyles();
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
 
     const { setIdReceta } = useContext(ModalContext);
 
@@ -24,10 +62,18 @@ const Receta = ({ receta }) => {
                         className="btn btn-block btn-primary"
                         onClick={() => {
                             setIdReceta(receta.idDrink)
+                            handleOpen();
                         }}
                     >
                         Ver Receta
                     </button>
+                    <Modal
+                        open={open}
+                    >
+                        <div style={modalStyle} className={classes.paper}>
+                            <h1>Desde Modal</h1>
+                        </div>
+                    </Modal>
                 </div>
             </div>
         </div>
